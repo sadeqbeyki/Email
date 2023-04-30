ab06b23e108e9030cd67b43011e969f01e71bf71
mardibak <mardibaki@gmail.com>
2023-05-01 02:54:36 +0330

    inbox implemented

---
+++ Email.MailC/Inbox.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Inbox.aspx.cs" Inherits="Email.MailC.Inbox" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="InboxGridView" runat="server" AutoGenerateColumns="False">
+                <Columns>
+                    <asp:BoundField DataField="SenderUsername" HeaderText="Sender" />
+                    <asp:BoundField DataField="MessageText" HeaderText="Message" />
+                    <asp:BoundField DataField="SentDate" HeaderText="Date" DataFormatString="{0:yyyy-MM-dd HH:mm:ss}" />
+                </Columns>
+            </asp:GridView>
+        </div>
+    </form>
+
+
+
+</body>
+</html>
---
+++ Email.MailC/Inbox.aspx.cs
@@ -0,0 +1,47 @@
+﻿using Email.MailC.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Email.MailC
+{
+    public partial class Inbox : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadInbox();
+            }
+        }
+        private void LoadInbox()
+        {
+            var userStore = new UserStore<IdentityUser>();
+            var userManager = new UserManager<IdentityUser>(userStore);
+            var users = userManager.Users.ToList();
+
+            using (var context = new MessageContext())
+            {
+                var currentUser = users.FirstOrDefault(u => u.UserName == User.Identity.Name);
+
+                if (currentUser != null)
+                {
+                    var receivedMessages = context.Messages.Where(msg=>msg.ReceiverId==currentUser.Id).ToList();
+
+                    InboxGridView.DataSource = receivedMessages.Select(m => new
+                    {
+                        SenderUsername = m.SenderUsername,
+                        MessageText = m.MessageText,
+                        SentDate = m.SentDate
+                    });
+                    InboxGridView.DataBind();
+                }
+            }
+        }
+    }
+}
